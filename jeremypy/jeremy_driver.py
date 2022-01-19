47da04c016983aa9f1f6ad7827a74a3d046576d2
Jeremy Hobson <jeremyhobson111@gmail.com>
2022-01-18 21:05:04 -0800

    Fixed user-agent argument

--- jeremypy/jeremy_driver.py
+++ jeremypy/jeremy_driver.py
@@ -20,7 +20,7 @@ class JeremyDriver:
         if profile_path:
             options.add_argument(f'--user-data-dir={profile_path}')
         options.add_argument(
-            'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+            '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
             'Chrome/87.0.4280.88 Safari/537.36')
         options.add_argument('--no-first-run')
         options.add_argument('--no-service-autorun')
